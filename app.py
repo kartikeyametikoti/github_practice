749a5d32db933c34f1e490fb9e713d9fa409e561
kartikeyametikoti <149247879+kartikeyametikoti@users.noreply.github.com>
2025-03-05 16:39:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello from Dockerized Elastic Beanstalk!"
+    return "Hello from  Dockerized Elastic Beanstalk!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
